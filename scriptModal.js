fb475c5b31607d2f3626b3d0055d61bbc17aa228
Frank-Pechar <93831849+Frank-Pechar@users.noreply.github.com>
2023-02-18 17:29:04 -0700

    Add files via upload

--- scriptModal.js
+++ scriptModal.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // const artModal = document.querySelector(".modal");
 // fetch("/index-art-modal.html")
@@ -7,7 +7,7 @@
 //     artModal.innerHTML = data;
 //   });
 
-const modal = document.querySelector(".modal");
+const modal = document.querySelector('.modal');
 modal.innerHTML = `    
   <div class="modalContent">
     <img src="" class="modalImg" />
@@ -20,58 +20,58 @@ modal.innerHTML = `
   </div>
 `;
 
-const images = document.querySelectorAll(".card__img-thumb");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
-const close = document.querySelector(".close");
-const prevBtn = document.querySelector(".prevBtn");
-const nextBtn = document.querySelector(".nextBtn");
+const images = document.querySelectorAll('.card__img-thumb');
+const modalImg = document.querySelector('.modalImg');
+const modalTxt = document.querySelector('.modalTxt');
+const close = document.querySelector('.close');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
 
 images.forEach((image, index) => {
-  image.addEventListener("click", () => {
+  image.addEventListener('click', () => {
     modalImg.src = image.src;
-    modalTxt.innerHTML = image.alt;
-    modal.classList.add("appear");
+    // modalTxt.innerHTML = image.alt;
+    modal.classList.add('appear');
     let imageIndex = index;
     let next = imageIndex + 1;
     let prev = imageIndex - 1;
 
-    window.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowLeft") {
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'ArrowLeft') {
         if (prev < 0) prev = images.length - 1;
         prevImg();
-      } else if (e.key === "ArrowRight") {
+      } else if (e.key === 'ArrowRight') {
         if (next >= images.length) next = 0;
         nextImg();
-      } else if (e.key === "Escape") {
-        modal.classList.remove("appear");
+      } else if (e.key === 'Escape') {
+        modal.classList.remove('appear');
       }
     });
 
-    prevBtn.addEventListener("click", () => {
+    prevBtn.addEventListener('click', () => {
       if (prev < 0) prev = images.length - 1;
       prevImg();
     });
 
-    nextBtn.addEventListener("click", () => {
+    nextBtn.addEventListener('click', () => {
       if (next >= images.length) next = 0;
       nextImg();
     });
 
-    close.addEventListener("click", () => {
-      modal.classList.remove("appear");
+    close.addEventListener('click', () => {
+      modal.classList.remove('appear');
     });
 
     function nextImg() {
       modalImg.src = images[next].src;
-      modalTxt.innerHTML = images[next].alt;
+      // modalTxt.innerHTML = images[next].alt;
       next++;
       prev = next - 2;
     }
 
     function prevImg() {
       modalImg.src = images[prev].src;
-      modalTxt.innerHTML = images[prev].alt;
+      // modalTxt.innerHTML = images[prev].alt;
       prev--;
       next = prev + 2;
     }
